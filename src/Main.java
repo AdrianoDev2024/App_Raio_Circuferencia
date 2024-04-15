1584b8fae0a87da4358dace9c18c45d5425334c6
Adriano Soares <adrianoprodevjava@gmail.com>
2024-04-14 22:07:44 -0300

    Crtiado app para calcular a area

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //final double PI= 3.1456;
+
+        System.out.println("Informe o valor do raio");
+        double raio = input.nextDouble();
+        double area = raio * raio * Math.PI;
+
+        System.out.println("O valor da Area " + area);
+    }
+}
